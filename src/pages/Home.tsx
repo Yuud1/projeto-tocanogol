97cc79d64613d3d04d297764def56fa5dcf533e4
Yuud1 <lucas.yudi@mail.uft.edu.br>
2024-12-09 19:52:48 -0300

    preparando ambiente

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <h1>Home</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
